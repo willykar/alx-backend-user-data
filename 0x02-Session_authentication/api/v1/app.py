28f32fc4d61e17cf24566352de228f79ef9cd154
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 21:51:14 +0300

    Update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,28 +2,25 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
 import os
+from os import getenv
+
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-# Initialize the auth variable
 auth = None
 
-# Load the appropriate authentication class based
-# on the environment variable AUTH_TYPE
-auth_type = os.getenv('AUTH_TYPE', 'auth')
-if auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif auth_type == 'auth':
-    from api.v1.auth.auth import Auth
+if getenv("AUTH_TYPE") == 'auth':
     auth = Auth()
+if getenv("AUTH_TYPE") == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.before_request
@@ -39,22 +36,23 @@ def before_request():
             abort(403)
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
-
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
-    return jsonify({"error": "Unauthorized"}), 401
+    """Not authorized handler"""
+    return jsonify({'error': 'Unauthorized'}), 401
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler """
-    return jsonify({"error": "Forbidden"}), 403
+    """forbidden handler"""
+    return jsonify({'error': 'Forbidden'}), 403
 
 
 if __name__ == "__main__":
