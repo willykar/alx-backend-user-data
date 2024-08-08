42954559ecdd092d99a437e253e0e7fde7724e2c
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 16:59:50 +0300

    Create basic_auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+basic_auth module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class that inherits from Auth """
+    pass
