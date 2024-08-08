d50a3046be3bce0eeea37b112e4e798b191e536d
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 16:28:40 +0300

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,10 +27,12 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Checks if Authorization request header is present
         and contains values"""
-        if request is None or "Authorization" not in request.headers:
+        if request is None:
             return None
-        else:
-            return request.headers.get('Authorization')
+            
+        if "Authorization" not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retrieves the current user based on the request """
