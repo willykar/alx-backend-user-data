a0d18c7dd31e32106e5425874c625931983f7344
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 23:08:04 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,52 +1,44 @@
 #!/usr/bin/env python3
 """
-session_authentication
+SessionAuth
 """
 
-
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from uuid import uuid4
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """ Session Authentication class """
-
+    """
+    Session auth class
+    """
     user_id_by_session_id = {}
 
-    
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session ID for a user_id"""
-        if user_id is None or not isinstance(user_id, str):
-            return None
-
-        session_id = str(uuid.uuid4())
-        self.user_id_by_session_id[session_id] = user_id
+        """
+        creates a Session ID for a user_id
+        """
+        if not user_id or type(user_id) != str:
+            return
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[user_id] = session_id
         return session_id
 
-    
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a user ID based on a session ID"""
-        if session_id is None or not isinstance(session_id, str):
-            return None
-        return self.user_id_by_session_id.get(session_id)
+        """
+        returns a User ID based on a Session ID
+        """
+        if not session_id or type(session_id) != str:
+            return
+        return SessionAuth.user_id_by_session_id.get(session_id, None)
 
-    
-    def session_cookie(self, request=None):
-        """Returns the session cookie value from a request"""
-        if request is None:
-            return None
-        session_name = os.getenv('SESSION_NAME')
-        return request.cookies.get(session_name)
-
-    
-    def current_user(self, request=None):
-        """Returns a User instance based on a session cookie"""
-        session_id = self.session_cookie(request)
-        if session_id is None:
-            return None
-
-        user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
-
-        return User.get(user_id)
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current_user.
+        """
+        if request:
+            session_cookie = self.session_cookie(request)
+            if session_cookie:
+                user_id = self.user_id_for_session_id(session_cookie)
+                return User.get(user_id)
