3389a024db35b23f1687b2799daab278ff56f7ea
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 23:21:56 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,77 +1,53 @@
 #!/usr/bin/env python3
-"""View Session Authentication Module"""
-
-from api.v1.views import app_views
-from flask import request, jsonify, abort
+"""
+A Flask view that handles all routes for the Session authentication.
+"""
+from flask import jsonify, request, abort
 from models.user import User
+from api.v1.views import app_views
 import os
-from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """You must use request.form.get() to retrieve email and password
-        parameters
-        If email is missing or empty, return the JSON
-        { "error": "email missing" } with the status code 400 If password is
-        missing or empty, return the JSON { "error": "password missing" }
-        with the status code 400 Retrieve the User instance based on the
-        email - you must use the class method search of User
-        (same as the one used for the BasicAuth) If no User found,
-        return the JSON { "error": "no user found for this email" }
-        with the status code 404 If the password is not the one of the
-        User found, return the JSON { "error": "wrong password" } with
-        the status code 401 - you must use is_valid_password from the
-        User instance Otherwise, create a Session ID for the User ID:
-        You must use from api.v1.app import auth - WARNING: please import
-        it only where you need it - not on top of the file
-        (can generate circular import - and break first tasks of this project)
-        You must use auth.create_session(..) for creating a Session ID
-        Return the dictionary representation of the User - you must use
-        to_json() method from User
-        You must set the cookie to the response - you must use the
-        value of the environment variable SESSION_NAME as cookie name - tip In
-        the file api/v1/views/__init__.py, you must add this
-        new view at the end of the file.
+def session_login():
     """
-    from api.v1.app import auth
-
+    Session Login
+    """
     email = request.form.get('email')
     password = request.form.get('password')
-    if email is None or email == "":
+
+    if not email:
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == "":
+    if not password:
         return jsonify({"error": "password missing"}), 400
-    users = User.search({"email": email})
-    if not users or len(users) == 0:
+
+    try:
+        user = User.search({"email": email})
+    except Exception:
         return jsonify({"error": "no user found for this email"}), 404
-    user = users[0]
-    if not user.is_valid_password(password):
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    for users in user:
+        if users.is_valid_password(password):
+            user_id = users.id
+            from api.v1.app import auth
+            session_id = auth.create_session(user_id)
+            response = jsonify(users.to_json())
+            response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+            return response
         return jsonify({"error": "wrong password"}), 401
-    session_id = auth.create_session(user.id)
-    response = jsonify(user.to_json())
-    session_name = os.getenv('SESSION_NAME')
-    response.set_cookie(session_name, session_id)
-    return response
+    return jsonify({"error": "no user found for this email"}), 404
 
 
-@app_views.route(
-    '/auth_session/logout', methods=['DELETE'], strict_slashes=False
-)
+@app_views.route('auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout():
-    """Handles the session logout: Update the class SessionAuth by adding a new
-        method def destroy_session(self, request=None): that deletes the user
-        session / logout: If the request is equal to None, return False
-        If the request doesn’t contain the Session ID cookie, return
-        False - you must use self.session_cookie(request) If the
-        Session ID of the request is not linked to any User ID, return
-        False - you must use self.user_id_for_session_id(...) Otherwise,
-        delete in self.user_id_by_session_id the Session ID
-        (as key of this dictionary) and return True
     """
+    Session Logout
+    """
+
     from api.v1.app import auth
-
-    if auth.destroy_session(request) is False:
+    if not auth.destroy_session(request):
         abort(404)
-
     return jsonify({}), 200
