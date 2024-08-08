d6b953d98927490c553ff733f30b1fedd15afb00
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 16:57:53 +0300

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,22 +15,23 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # Initialize the auth variable
 auth = None
 
-# Load the appropriate authentication class based on the environment variable
+# Load the appropriate authentication class based on the environment variable AUTH_TYPE
 auth_type = os.getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-
 
 @app.before_request
 def before_request():
     """Filter each request before routing it to the corresponding view"""
     if auth is None:
         return
-
+    
     # Paths that do not require authentication
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     # Check if the request path requires authentication
     if not auth.require_auth(request.path, excluded_paths):
@@ -44,24 +45,20 @@ def before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
