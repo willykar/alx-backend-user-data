9938961cfd05a8526d864672858a30785c805c95
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 22:03:42 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,44 +1,14 @@
 #!/usr/bin/env python3
 """
-SessionAuth
+SessionAuth module for handling session-based authentication.
 """
 
 from api.v1.auth.auth import Auth
-from typing import TypeVar
-from uuid import uuid4
-from models.user import User
 
 
 class SessionAuth(Auth):
     """
-    Session auth class
+    SessionAuth class that inherits from Auth.
+    This class will handle session-based authentication.
     """
-    user_id_by_session_id = {}
-
-    def create_session(self, user_id: str = None) -> str:
-        """
-        creates a Session ID for a user_id
-        """
-        if not user_id or type(user_id) != str:
-            return
-        session_id = str(uuid4())
-        SessionAuth.user_id_by_session_id[user_id] = session_id
-        return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """
-        returns a User ID based on a Session ID
-        """
-        if not session_id or type(session_id) != str:
-            return
-        return SessionAuth.user_id_by_session_id.get(session_id, None)
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """
-        current_user.
-        """
-        if request:
-            session_cookie = self.session_cookie(request)
-            if session_cookie:
-                user_id = self.user_id_for_session_id(session_cookie)
-                return User.get(user_id)
+    pass
