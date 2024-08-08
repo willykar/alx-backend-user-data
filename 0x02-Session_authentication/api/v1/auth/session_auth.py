08cdeb726335893e311898a05b2b78426d83d9f8
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 22:07:12 +0300

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,11 +4,36 @@ SessionAuth module for handling session-based authentication.
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     SessionAuth class that inherits from Auth.
-    This class will handle session-based authentication.
+    Handles session-based authentication.
     """
-    pass
+    # Class attribute to store user_id by session_id
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id.
+        
+        Args:
+            user_id (str): The ID of the user for whom the
+            session is being created.
+        
+        Returns:
+            str: The generated Session ID, or None if user_id is invalid.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        # Generate a unique Session ID using uuid4
+        session_id = str(uuid.uuid4())
+
+        # Store the session_id and associated user_id in the 
+        # class attribute dictionary
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
