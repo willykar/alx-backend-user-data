687c60ad6c17f6f49b611c3bb24dc2ca34c50ce6
willykar <103494595+willykar@users.noreply.github.com>
2024-08-08 23:19:52 +0300

    Update __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,12 +1,53 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
 """
-from flask import Blueprint
+A Flask view that handles all routes for the Session authentication.
+"""
+from flask import jsonify, request, abort
+from models.user import User
+from api.v1.views import app_views
+import os
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.users import *
-from api.v1.views.session_auth import *
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login():
+    """
+    Session Login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
 
-User.load_from_file()
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        user = User.search({"email": email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    for users in user:
+        if users.is_valid_password(password):
+            user_id = users.id
+            from api.v1.app import auth
+            session_id = auth.create_session(user_id)
+            response = jsonify(users.to_json())
+            response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+            return response
+        return jsonify({"error": "wrong password"}), 401
+    return jsonify({"error": "no user found for this email"}), 404
+
+
+@app_views.route('auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """
+    Session Logout
+    """
+
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
