e4fd4d352de1e785b089704869b3484cecb59f6e
willykar <wilsonnjenga799@gmail.com>
2024-08-13 05:07:02 -0700

    update_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,3 +92,16 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError("User not found")
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hash_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hash_password,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError("User doesn't exist")
