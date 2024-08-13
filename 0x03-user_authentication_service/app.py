def9b361965b46b41fc8331ef8f22bb823781737
willykar <wilsonnjenga799@gmail.com>
2024-08-13 04:16:47 -0700

    get_reset_password_token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -72,5 +72,18 @@ def profile():
         abort(403)
 
 
+@app.route("/reset_password", methods=["GET"])
+def get_reset_password_token() -> str:
+    """
+    reset_password route
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
