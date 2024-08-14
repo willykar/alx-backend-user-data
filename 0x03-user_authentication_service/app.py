007c7cbe00298b1df7135956bf519aa8b721d5bc
willykar <103494595+willykar@users.noreply.github.com>
2024-08-14 12:20:00 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route("/reset_password", methods=["PUT"])
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password():
     """
     update_password function
