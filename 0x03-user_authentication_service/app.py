408c0c28f5df5b7f6f89617c4f9ca9fda707e846
willykar <wilsonnjenga799@gmail.com>
2024-08-14 00:30:56 -0700

    update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,7 +96,7 @@ def update_password() -> str:
     try:
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
-    except: NoResultFound:
+    except NoResultFound:
         abort(403)
 
 
