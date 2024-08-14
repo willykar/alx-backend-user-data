5e43ce28e02104248c438da663b729beef3548a8
willykar <103494595+willykar@users.noreply.github.com>
2024-08-14 12:22:22 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,7 +101,7 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
-        abort(403)
+        return abort(403)
 
 
 if __name__ == "__main__":
