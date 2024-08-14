1437a87b63d01d8134d48a3ec9f27ef8daba7a91
willykar <103494595+willykar@users.noreply.github.com>
2024-08-14 11:58:27 +0300

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,6 +67,8 @@ def profile():
     Get profile of a user
     """
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403) 
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email}), 200
